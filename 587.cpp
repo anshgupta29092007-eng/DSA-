884856b99d71a9f6986f121a539d1233de716de4
Ansh Gupta <anshgupta29092007@gmail.com>
2026-08-11 23:59:57 +0530

    Create 587.cpp

---
+++ 587.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> addToArrayForm(vector<int>& num, int k) {
+
+        for (int i = num.size() - 1; i >= 0; i--) {
+            k += num[i];
+            num[i] = k % 10;
+            k /= 10;
+        }
+
+        for (; k > 0; k /= 10) {
+            num.insert(num.begin(), k % 10);
+        }
+
+        return num;
+    }
+};
